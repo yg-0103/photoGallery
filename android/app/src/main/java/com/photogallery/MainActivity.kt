a0cddb29dd1fb2ce5e70e90c52547ea19a4f10b8
김연구 <yeongoo.kim@qualson.com>
2022-01-17 21:09:59 +0900

    initial setting

---
+++ android/app/src/main/java/com/photogallery/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.photogallery
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "photoGallery"
+    }
+}
